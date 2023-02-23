d1bf94137f641dcd6e1296546ab8efcc6d5dc409
Grosche510 <grosche510@gmail.com>
2023-02-23 12:58:33 +0300

    add README.md

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
+import prompt
+def greet():
+	print('Welcome to the Brain Games!')
 
 def main():
-    print('Welcome to the Brain Games!')
-
+	greet()
 
 if __name__ == '__main__':
-    main()
+	greet()
 
+welcome_user()
