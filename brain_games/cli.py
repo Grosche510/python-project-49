511156c2444ae0f0b18939447190420d70917100
Grosche510 <grosche510@gmail.com>
2023-02-26 09:27:21 +0300

    cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,5 +2,5 @@ import prompt
 
 
 def welcome_user():
-	name = prompt.string('May I have your name? ')
-	return (f'Hello, {name}!')
+    name = prompt.string('May I have your name? ')
+    return (f'Hello, {name}!')
