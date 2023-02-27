6eceb6a7da3a0547d896d927625fe366b08ccd32
Grosche510 <grosche510@gmail.com>
2023-02-27 08:58:07 +0300

    calc.py

---
+++ brain_games/games/calc.py
@@ -0,0 +1,18 @@
+from random import randint
+import random
+RULE = 'What is the result of the expression?'
+
+
+def generate_data():
+    operator = ['+', '-', '*']
+    number1 = randint(1, 10)
+    number2 = randint(1, 10)
+    operator = random.choice(operators)
+    question = f"{number1} {operator} {number2}"
+    if operator == '+':
+        right_answer = number1 + number2
+    elif operator == '-':
+         right_answer = number1 - number2
+    elif operator == '*':
+         right_answer = number1 * number2
+    return question, str(right_answer)
