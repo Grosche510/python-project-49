21d3c8512ed82c119c033bca707dc66b4f1fc000
Grosche510 <grosche510@gmail.com>
2023-02-27 09:52:31 +0300

    gcd.py

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,11 @@
+from random import randint
+import math
+RULE = 'Find the greatest common divisor of given numbers.'
+
+
+def generate_data():
+    number1 = randint(1, 100)
+    number2 = randint(1, 100)
+    question = f'{number1} {number2}'
+    right_answer = str(math.gcd(number1, number2))
+    return question, right_answer
