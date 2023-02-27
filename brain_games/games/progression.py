d77172a6bb5605ee85544da5948d8249c85d34fb
Grosche510 <grosche510@gmail.com>
2023-02-27 17:28:55 +0300

    progression.py

---
+++ brain_games/games/progression.py
@@ -0,0 +1,14 @@
+from random import randint
+RULE = 'What number is missing in the progression?'
+
+
+def generate_data():
+    start  = randint(1, 20)
+    step = randint(1, 10)
+    result = list(range(randint(0, 9), start, step))[ :5]
+    random_index = randint(0, len(result) -1)
+    right_answer = result[random_index]
+    result[random_index] = '..'
+    question = '..'.join(map(str, (result)))
+    result question, str(right_answer)
+
