25b3245929d6b8b1b89db0390db345f1b687dc1d
Grosche510 <grosche510@gmail.com>
2023-02-28 10:53:34 +0300

    prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,6 +1,6 @@
 from random import randint
 import math
-RULE = 'Answer "yes" if given number is prime, otherwise answer "no".'
+RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
