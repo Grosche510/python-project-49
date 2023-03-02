6d807e50a42b1ce5e4190ffcb30c18e763884231
Grosche510 <grosche510@gmail.com>
2023-03-02 17:48:09 +0300

    calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -9,18 +9,18 @@ OPERATION2 = '-'
 OPERATION3 = '*'
 
 def game_plan():
-    operator =choice([OPERATION1, OPERATION2, OPERATION3])
+    operation =choice([OPERATION1, OPERATION2, OPERATION3])
     number1 = randint(MIN_RANGE, MAX_RANGE)
     number2 = randint(MIN_RANGE, MAX_RANGE)
-    question = f"{number1} {operator} {number2}"
-    right_answer = str(calculate(number1, number2, operator))
+    question = f"{number1} {operation} {number2}"
+    right_answer = str(calculate(number1, number2, operation))
     return question, right_answer
 
-def calculate(number1, number2, operations):
-    if operator == '+':
-        result = number1 + number2
-    elif operator == '-':
-        result = number1 - number2
-    elif operator == '*':
-        result = number1 * number2
+def calculate(number1, number2, operation):
+    if operation == OPERATION1:
+        result = number1 OPERATION1 number2
+    elif operation == OPERATION2:
+        result = number1 OPERATION2 number2
+    elif operation == OPERATION3:
+        result = number1 OPERATION3 number2
     return result
