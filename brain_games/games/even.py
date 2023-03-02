c538d71b268d0c5ab93569feb58f298fdde1696e
Grosche510 <grosche510@gmail.com>
2023-03-02 15:41:47 +0300

    even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,9 +1,11 @@
 from random import randint
-RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
+RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
+MIN_RANGE = 1
+MAX_RANGE = 10
 
 def game_plan():
-    question = randint(1, 10)
+    question = randint(MIN_RANGE, MAX_RANGE)
     if question % 2 == 0:
         right_answer = 'yes'
     elif question % 2 != 0:
