15105c07b761e8ae557e203fb54f39b44beed6b9
Grosche510 <grosche510@gmail.com>
2023-03-02 15:06:40 +0300

    calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,12 +1,17 @@
 from random import randint
 import random
-RULE = 'What is the result of the expression?'
 
+RULE = 'What is the result of the expression?'
+MIN_RANGE = 1
+MAX_RANGE = 10
+OPERATION1 = '+'
+OPERATION2 = '-'
+OPERATION3 = '*'
 
 def game_plan():
-    operations = ['+', '-', '*']
-    number1 = randint(1, 10)
-    number2 = randint(1, 10)
+    operations = [OPERATION1, OPERATION2, OPERATION3]
+    number1 = randint(MIN_RANGE, MAX_RANGE)
+    number2 = randint(MIN_RANGE, MAX_RANGE)
     operator = random.choice(operations)
     question = f"{number1} {operator} {number2}"
     if operator == '+':
