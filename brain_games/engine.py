f04bfa50d13f9a168d00bf96a88219a2ed298c3e
Grosche510 <grosche510@gmail.com>
2023-03-02 16:49:33 +0300

    engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -13,7 +13,7 @@ def play(game):
         answer = prompt.string('Your answer: ')
         if answer == right_answer:
             return('Correct!')
-        if (f'\"{answer}\" is wrong answer ;(.'f' Correct answer was "{right_answer}".')
+        if (f'\"{answer}\" is wrong answer ;(.'f' Correct answer was "{right_answer}".'):
             return(f"Let's try again, {name}!")
             break
         return(f'Congratulations, {name}!')
