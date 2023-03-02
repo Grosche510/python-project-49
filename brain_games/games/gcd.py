59b13e21e50467da4b0b606bd72370bb810d1eb9
Grosche510 <grosche510@gmail.com>
2023-03-02 15:48:34 +0300

    gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,11 +1,14 @@
 from random import randint
 import math
+
+MIN_RANGE = 1
+MAX_RANGE = 100
 RULE = 'Find the greatest common divisor of given numbers.'
 
 
 def game_plan():
-    number1 = randint(1, 100)
-    number2 = randint(1, 100)
+    number1 = randint(MIN_RANGE, MAX_RANGE)
+    number2 = randint(MIN_RANGE, MAX_RANGE)
     question = f'{number1} {number2}'
     right_answer = str(math.gcd(number1, number2))
     return question, right_answer
