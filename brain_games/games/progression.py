fc0ff2dfeba3057bea391d94899f0f9f3c4d7491
Grosche510 <grosche510@gmail.com>
2023-03-03 10:14:00 +0300

    progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,22 +1,28 @@
 from random import randint
 
-MIN_STOP = 50
-MAX_STOP = 100
+MIN_START = 1
+MAX_START = 30
 MIN_INTERVAL = 1
-MAX_INTERVAL = 5
+MAX_INTERVAL = 10
+MIN_LENGTH = 5
+MAX_LENGTH = 10
 RULE = 'What number is missing in the progression?'
 
 
-def our_progression(stop, interval, result):
-    result = list(range(randint(0, 9), stop, interval))[: 5]
-    return result
+def our_progression(start, interval, length):
+    stop = start + (length * interval)
+    numbers_list = []
+    for index in range(start, stop, interval):
+        numbers_list.append(index)
+    return numbers_list
 
 def game_plan():
-    stop = randint(MIN_STOP, MAX_STOP)
+    start = randint(MIN_START, MAX_START)
     interval = randint(MIN_INTERVAL, MAX_INTERVAL)
-    result = our_progression(stop, interval, result)
-    hidden_index = randint(0, len(result) - 1)
-    right_answer = str(result[hidden_index])
-    result[hidden_index] = '..'
-    question = ' '.join(map(str, result))
+    length =  randint(MIN_LENGTH, MAX_LENGTH)
+    progression = our_progression(start, interval, length)
+    hidden_index = randint(0, len(progression) - 1)
+    right_answer = str(progression[hidden_index])
+    progression[hidden_index] = '..'
+    question = ' '.join(map(str, progression))
     return question, right_answer
