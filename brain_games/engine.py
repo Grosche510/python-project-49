27f12dc763c149fcc6ccbe2e05d7515ea0df02ec
Grosche510 <grosche510@gmail.com>
2023-03-03 09:27:28 +0300

    engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -13,7 +13,7 @@ def play(game):
         print(f"Question: {question}")
         answer = prompt.string('Your answer: ')
 
-        if rigth_answer == answer.lower():
+        if right_answer == answer.lower():
            print('Correct!')
         else:
            print(f'\"{answer}\" is wrong answer ;(.'f' Correct answer was "{right_answer}".')
